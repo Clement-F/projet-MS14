ee795c15f9f6ffbbb790db7f9d6de72b7a62b7ce
Clement <clement.fontan@outlook.com>
2026-02-24 20:39:22 +0100

    hotfix

--- projet-MS14/src/main_mesh.c
+++ projet-MS14/src/main_mesh.c
@@ -35,12 +35,17 @@ int main(int argc, char* argv[])
   printf("  time hash tab neigh.  %lg (s) \n", (ti - to) / CLOCKS_PER_SEC);
 
   //--- TODO: compute mesh quality
+
+  printf("Quality evaluation :\n");
   double* Qal = (double*)malloc(sizeof(double) * (Msh->NbrTri + 1));
 
   double alpha = sqrt(3)/12;
+  double K_surf;
+
+  int i1,i2,i3; double a,b,c;
 
   for (iTri = 1; iTri <= Msh->NbrTri; iTri++) {
-  int i1,i2,i3; double a,b,c;
+  printf("calc %d \n",iTri);
   i1 = Msh->Tri[iTri][0];        i2 = Msh->Tri[iTri][1];        i3 = Msh->Tri[iTri][2];
   double2d P1 = {Msh->Crd[i1][0], Msh->Crd[i1][1] };
   double2d P2 = {Msh->Crd[i2][0], Msh->Crd[i2][1] };
@@ -50,7 +55,8 @@ int main(int argc, char* argv[])
   b = (P2[0]-P3[0])*(P2[0]-P3[0]) + (P2[1]-P3[1])*(P2[1]-P3[1]); 
   c = (P3[0]-P1[0])*(P3[0]-P1[0]) + (P3[1]-P1[1])*(P3[1]-P1[1]); 
   double K_surf = 0.5*((P2[0]-P1[0])*(P3[1]-P1[1]) - (P1[1]-P2[1])*(P3[0]-P1[0]));
-    Qal[iTri] = (a+b+c)/K_surf;
+
+  Qal[iTri] = (a+b+c)/K_surf; printf("%d quality evaluated at : %f \n", iTri, Qal[iTri]);
   }
 
   msh_write2dfield_Triangles("quality.solb", Msh->NbrTri, Qal);
